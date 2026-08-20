3741b0c13790c7a64521f479a14c21ccbfb2ed53
vanshikaGarg2405 <gargm4932@gmail.com>
2026-08-20 23:03:48 +0530

    Arrays

---
+++ Arrays/rearrangeElementsBySign.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// positive and negative elements are equal
+vector<int> rearrangeElementsBrute(vector<int> &arr, int n) {
+    vector<int>pos, neg;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > 0) pos.push_back(arr[i]);
+        else neg.push_back(arr[i]);
+    }
+    for(int i = 0; i < n / 2; i++) {
+        arr[2 * i] = pos[i];
+        arr[2 * i + 1] = neg[i];
+    }
+    return arr;
+}
+
+vector<int> rearrangeElementsOptimal(vector<int>& arr, int n) {
+    vector<int> ans(n);
+    int posIndex = 0, negIndex = 1;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] < 0) {
+            ans[negIndex] = arr[i];
+            negIndex += 2;
+        }
+        else {
+            ans[posIndex] = arr[i];
+            posIndex += 2;
+        }
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cout << "Enter n: ";
+    cin >> n; 
+    vector<int> arr(n);
+    cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    vector<int> ans = rearrangeElementsOptimal(arr, n);
+    for(auto x : ans) cout << x << " ";
+    return 0;
+}
